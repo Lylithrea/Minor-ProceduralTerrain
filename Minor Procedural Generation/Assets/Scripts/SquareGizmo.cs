78f29676294c3a3683c46fcde0ace6a68b1898ce
Lylithrea <ezraduss@gmail.com>
2021-09-06 15:07:41 +0200

    Initial commit

---
+++ Minor Procedural Generation/Assets/Scripts/SquareGizmo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareGizmo : MonoBehaviour
+{
+
+    void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(new Vector3(0,0,0), new Vector3(0,0,1));
+        Gizmos.DrawLine(new Vector3(0,0,0), new Vector3(0,1,0));
+        Gizmos.DrawLine(new Vector3(0,0,0), new Vector3(1,0,0));
+        Gizmos.DrawLine(new Vector3(1,1,1), new Vector3(0,1,1));
+        Gizmos.DrawLine(new Vector3(1,1,1), new Vector3(1,1,0));
+        Gizmos.DrawLine(new Vector3(1,1,1), new Vector3(1,0,1));
+
+        Gizmos.DrawLine(new Vector3(1,0,0), new Vector3(1,1,0));
+        Gizmos.DrawLine(new Vector3(1,0,0), new Vector3(1,0,1));
+
+        Gizmos.DrawLine(new Vector3(0,1,0), new Vector3(1,1,0));
+        Gizmos.DrawLine(new Vector3(0,1,0), new Vector3(0,1,1));
+
+        Gizmos.DrawLine(new Vector3(0,0,1), new Vector3(0,1,1));
+        Gizmos.DrawLine(new Vector3(0,0,1), new Vector3(1,0,1));
+    }
+
+}
