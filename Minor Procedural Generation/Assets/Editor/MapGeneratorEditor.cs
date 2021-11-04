18b86a6d4f7e9b64a229d74f1c24ff85a7bf69b4
Lylithrea <ezraduss@gmail.com>
2021-11-04 14:37:44 +0100

    Added perlin noise 2D

---
+++ Minor Procedural Generation/Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        //checks if any values has changed
+        if(DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
---
+++ Minor Procedural Generation/Assets/Scripts/Final/MapGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+    public bool autoUpdate = false;
+
+    public Texture2D texture;
+    public Material mat;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = PlanePerlin.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        //float[,] map = texture.GetPixels();
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+        //Texture mainTex = mat.mainTexture;
+        //Texture2D newTex = new Texture2D(mainTex.width, mainTex.height);
+
+        //display.SetTexture((Texture2D)mat.GetTexture("surface"));
+
+    }
+
+
+    
+}
