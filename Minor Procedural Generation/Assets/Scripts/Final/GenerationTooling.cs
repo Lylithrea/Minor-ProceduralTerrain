b94bb541fb4fbf979c8c910509a870039c5b0dc7
Lylithrea <ezraduss@gmail.com>
2021-11-22 09:57:29 +0100

    Working interpolation on 2D and 3D perlin noise

--- Minor Procedural Generation/Assets/Scripts/Final/GenerationTooling.cs
+++ Minor Procedural Generation/Assets/Scripts/Final/GenerationTooling.cs
@@ -14,12 +14,16 @@ public class GenerationTooling : MonoBehaviour
     //[Range(0.43f, 0.47f)]
     public float scale;
     public float groundLevelHeight = 250;
-    public float frequency = 25;
+    public int groundLevel;
+    [Range(0,1f)] [Tooltip("Influences how much value the next octave brings")]
+    public float persistence = 0.65f;
+    [Range(0, 1f)] [Tooltip("Influences the frequency/noise of the next octave")]
+    public float lacunarity = 0.25f;
+    [Range(1, 8)][Tooltip("Influences how many layers of noise go on top of each other")]
+    public int octaves = 4;
 
     [Header("Generation Settings")]
     public int radius;
-    public int groundLevel;
-    public int layerThickness;
     public float cutoff;
 
     [Header("Chunk Settings")]
@@ -93,14 +97,14 @@ public class GenerationTooling : MonoBehaviour
         noiseShader.SetFloat("noiseScale", scale);
         noiseShader.SetFloat("repeat", 9);
         noiseShader.SetInt("pointsPerAxis", pointsPerAxis);
+        noiseShader.SetFloat("groundHeight", groundLevel);
+        noiseShader.SetFloat("mountainHeight", groundLevelHeight);
+        noiseShader.SetFloat("persistence", persistence);
+        noiseShader.SetFloat("lacunarity", lacunarity);
+        noiseShader.SetInt("octaves", octaves);
 
         marchingCubeShader.SetInt("pointsPerAxis", pointsPerAxis);
         marchingCubeShader.SetFloat("cutoff", cutoff);
-        marchingCubeShader.SetFloat("groundLevel", groundLevel);
-        marchingCubeShader.SetFloat("layerThickness", layerThickness);
-        marchingCubeShader.SetTexture(0, "groundTex", groundTexture);
-        marchingCubeShader.SetFloat("height", groundLevelHeight);
-        marchingCubeShader.SetFloat("frequency", frequency);
 
         Perlin.noiseShader = noiseShader;
         Perlin.pointsPerAxis = pointsPerAxis;
